883a9da3b0789361ff9e07373deb852854cad1f6
Sunil Kumar <sunilk@360logica.com>
2017-09-12 15:44:19 +0530

    Checkout the project on Git

---
+++ src/test/java/com/flipkart/constant/GlobalConstant.java
@@ -0,0 +1,20 @@
+package com.flipkart.constant;
+
+public class GlobalConstant {
+	
+	public enum FileNames {
+		TestDataRelativePath("src/test/resources/testdata/"),
+		UserProperties("User"),
+		TestdataProperties("TestData");
+				
+		private String value;
+		private FileNames(String value) {
+			this.value = value;
+		}
+		
+		public String toString() {
+			return value;
+		}
+	}
+
+}
